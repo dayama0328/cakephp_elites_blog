110163c2a47290f6d0f9563ebf88af7a21f1b150
dayama <yamapoon24@gmail.com>
2016-01-08 19:21:33 +0900

    postモデルを変更した

--- app/Model/Post.php
+++ app/Model/Post.php
@@ -4,17 +4,19 @@ class Post extends AppModel {
 
   public $validate = array(
         'title' => array(
-            'rule' => array('maxLength', '20'),
-            'required' => true,
-            'message' => 'タイトルは20文字以内で入力してください'
+            'maxLength' => array(
+                'rule' => array('maxLength', '20'),
+                'required' => true,
+                'message' => 'タイトルは20文字以内で入力してください',
+                'style' => 'color:red;'
             ),
-
-         'content' => array(
-            'rule' => array('maxLength', '20'),
+        ),
+        'content' => array(
+            'rule' => 'notBlank',
             'required' => true,
-            'message' => 'タイトルは20文字以内で入力してください'
-            )
-        );
+            'message' => '記事の内容を入力してください！'
+        )
+  );
 }
 
 ?>
